7330152fe317eb74a408986df93fdf83ed6e4577
Ali Paikan <ali.paikan@luxai.com>
2021-01-22 12:43:25 +0100

    adding new autostart script, implementation of gspeech

--- examples/qt_gspeech_interface/autostart/start_qt_gspeech_interface.sh
+++ examples/qt_gspeech_interface/autostart/start_qt_gspeech_interface.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 
 source /home/qtrobot/robot/autostart/qt_robot.inc
-export GOOGLE_APPLICATION_CREDENTIALS="/home/qtrobot/robot/code/tutorials/examples/qt_gspeech_interface/instance/service.json"
-
 SCRIPT_NAME="start_qt_gspeech_interface"
 LOG_FILE=$(prepare_logfile "$SCRIPT_NAME")
 
 {
 prepare_ros_environment
 wait_for_ros_node "/rosout" 60
-rosrun qt_gspeech_interface qt_gspeech_service.py
+
+exec echo "qtrobot" | sudo -kSi GOOGLE_APPLICATION_CREDENTIALS="/home/qtrobot/robot/code/tutorials/examples/qt_gspeech_interface/service.json" PYTHONPATH=${PYTHONPATH}:/home/qtrobot/.local/lib/python3.5/site-packages/:/home/qtrobot/catkin_ws/devel/lib/python2.7/dist-packages/  python3 /home/qtrobot/robot/code/tutorials/examples/qt_gspeech_interface/src/qt_gspeech_service.py
 
 } &>> ${LOG_FILE}
+
--- examples/qt_gspeech_interface/src/qt_gspeech_service.py
+++ examples/qt_gspeech_interface/src/qt_gspeech_service.py
@@ -9,6 +9,43 @@ from google.cloud.speech import types
 from google.api_core import exceptions as gexcp
 from qt_gspeech_interface.srv import *
 
+
+class MicrophoneStream(object):
+
+    def __init__(self, buffer):
+        self.stream_buff = buffer
+        self.closed = True
+
+    def __enter__(self):
+        self.closed = False
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.closed = True
+        self.stream_buff.put(None)
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self.stream_buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self.stream_buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+
+
 class QTrobotGoogleSpeech(object):
     """QTrobot speech recognition using google cloud service"""
 
@@ -41,11 +78,10 @@ class QTrobotGoogleSpeech(object):
 
     def __del__(self):
         try:
-            self.stream.stop_stream()
             self.stream.close()
             self.paudio.terminate()
-        except:
-            pass
+        except Exception as e:
+            rospy.logfatal("could not close the service", e)
 
     """
         Get ReSpeaker microphone device
@@ -68,7 +104,7 @@ class QTrobotGoogleSpeech(object):
         microphone stream callback
     """
     def callback_stream(self, in_data, frame_count, time_info, status):
-        print("received:", len(in_data), frame_count, time_info, status)
+        #print("received:", len(in_data), frame_count, time_info, status)
         self.stream_buff.put(in_data)
         return None, pyaudio.paContinue
 
@@ -77,7 +113,10 @@ class QTrobotGoogleSpeech(object):
         ros speech recognize callback
     """
     def callback_recognize(self, req):
-        print(len(req.options), req.options)
+        self.stream.start_stream()
+        print("options:", len(req.options), req.options)
+        print("language:", req.language)
+        print("timeout:", str(req.timeout))
         answer_context = []
         speech_context = None
         for option in req.options:
@@ -87,28 +126,56 @@ class QTrobotGoogleSpeech(object):
         config = types.RecognitionConfig(
             encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
             sample_rate_hertz=16000,
-            language_code=req.language.strip() if req.language.strip() else "en-US",
+            language_code= str(req.language.strip()) if req.language.strip() else "en-US",
             speech_contexts = speech_context)
 
         streaming_config = types.StreamingRecognitionConfig(config=config, interim_results=True)
-        self.stream.start_stream()
-        time.sleep(2)
-        chunk = self.stream_buff.get()
-        data = [chunk]
-        print(len(data))
-        # Now consume whatever other data's still buffered.
-        while True:
+        with MicrophoneStream(self.stream_buff) as mic:
+            audio_generator = mic.generator()
+            requests = (
+                types.StreamingRecognizeRequest(audio_content=content)
+                for content in audio_generator
+            )
             try:
-                chunk = self.stream_buff.get(block=False)
-                data.append(chunk)
-            except queue.Empty:
-                break
-        print(len(data))
-        #request = types.StreamingRecognizeRequest(audio_content=b'...')
+                #print(requests)
+                responses = self.client.streaming_recognize(streaming_config, requests, timeout=(req.timeout if (req.timeout != 0) else 30))
+                #print('responses', responses)
+                output = self.validate_response(responses, answer_context)
+            except gexcp.DeadlineExceeded as e:
+                output = "#TIMEOUT#"
+                print("#TIMEOUT#")
         self.stream.stop_stream()
-        self.stream_buff.put(None)
+        print("Detected [%s]" % (output))
+        return QTrobotGspeechResponse(output)
 
-        return QTrobotGspeechResponse("hello")
+
+    """
+        looping over google responses
+    """
+    def validate_response(self, responses, context):
+        # to return:
+        # output = transcript
+        transcript = ""
+
+        #num_chars_printed = 0
+        for response in responses:
+            if not response.results:
+                continue
+            result = response.results[0]
+            if not result.alternatives:
+                continue
+            transcript = result.alternatives[0].transcript
+            if not result.is_final:
+                pass
+                 # if context:
+                 #     for option in context:
+                 #         if option == transcript.lower().strip():
+                 #             return transcript
+            else:
+                 return transcript
+
+        return transcript
+
 
 
 if __name__ == "__main__":
